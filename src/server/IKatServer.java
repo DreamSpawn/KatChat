c10b3f611bef079755111beb99ea54a762ae3f9c
JoachimØstergaard <jokses@gmail.com>
2016-04-12 12:33:07 +0200

    Katchat

--- src/server/IKatServer.java
+++ src/server/IKatServer.java
@@ -15,30 +15,12 @@ public class IKatServer {
 
     public interface GalgeI extends java.rmi.Remote {
 
-        public ArrayList<String> getBrugteBogstaver() throws java.rmi.RemoteException;
+        public int login(String name, String password) throws java.rmi.RemoteException;
 
-        public String login() throws java.rmi.RemoteException;
+        public String getMessage( int session) throws java.rmi.RemoteException;
 
-        public String getOrdet() throws java.rmi.RemoteException;
+        public void sentMessage(String message, int session) throws java.rmi.RemoteException;
 
-        public int getAntalForkerteBogstaver() throws java.rmi.RemoteException;
-
-        public boolean erSidsteBogstavKorrekt() throws java.rmi.RemoteException;
-
-        public boolean erSpilletVundet() throws java.rmi.RemoteException;
-
-        public boolean erSpilletTabt() throws java.rmi.RemoteException;
-
-        public boolean erSpilletSlut() throws java.rmi.RemoteException;
-
-        public void nulstil() throws java.rmi.RemoteException;
-
-        public void gætBogstav(String bogstav) throws java.rmi.RemoteException;
-
-        public void logStatus() throws java.rmi.RemoteException;
-
-        public String getMessage() throws java.rmi.RemoteException;
-    ;
 
 }
 }
