306182de5a70aa512d24c36a7dca7f4f6679755a
JoachimØstergaard <jokses@gmail.com>
2016-04-12 12:22:32 +0200

    Katchat

---
+++ src/server/KatChatI.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author JoachimØstergaard
+ */
+public class KatChatI {
+
+public interface GalgeI extends java.rmi.Remote
+{
+	 public ArrayList<String> getBrugteBogstaver()   throws java.rmi.RemoteException;
+	public String getSynligtOrd()                    throws java.rmi.RemoteException;
+	 public String getOrdet()                        throws java.rmi.RemoteException;
+       public int getAntalForkerteBogstaver()            throws java.rmi.RemoteException;
+       public boolean erSidsteBogstavKorrekt()           throws java.rmi.RemoteException;
+    public boolean erSpilletVundet() throws java.rmi.RemoteException;
+    public boolean erSpilletTabt() throws java.rmi.RemoteException;
+    public boolean erSpilletSlut() throws java.rmi.RemoteException;
+     public void nulstil() throws java.rmi.RemoteException;
+     public void gætBogstav(String bogstav)throws java.rmi.RemoteException;
+      public void logStatus() throws java.rmi.RemoteException;
+        public String getMessage() throws  java.rmi.RemoteException;;
+
+
+
+}   
+}
