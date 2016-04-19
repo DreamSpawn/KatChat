0febe1304589c1c80fda92f297cb637f892a2696
Mikkel Mikkel <westh_mikkel@hotmail.com>
2016-04-19 15:44:43 +0200

    webserver prototype

--- KatChat Webserver/src/java/webserver/Messages.java
+++ KatChat Webserver/src/java/webserver/Messages.java
@@ -27,27 +27,17 @@ import server.JavaServer;
 public class Messages extends HttpServlet {
 
 	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-	 * methods.
+	 * Handles the HTTP <code>GET</code> method.
 	 *
 	 * @param request servlet request
 	 * @param response servlet response
 	 * @throws ServletException if a servlet-specific error occurs
 	 * @throws IOException if an I/O error occurs
 	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		/*try {
-			IKatServer java_server = (IKatServer) Naming.lookup(JavaServer.completeAdress());
-		} catch (NotBoundException ex) {
-			Logger.getLogger(Messages.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (MalformedURLException ex) {
-			Logger.getLogger(Messages.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (RemoteException ex) {
-			Logger.getLogger(Messages.class.getName()).log(Level.SEVERE, null, ex);
-		}*/
-
+	
 		Object gate = new Object();
 		synchronized (gate) {
 			try {
@@ -63,21 +53,6 @@ public class Messages extends HttpServlet {
 		}
 	}
 
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
-
 	/**
 	 * Handles the HTTP <code>POST</code> method.
 	 *
@@ -89,7 +64,6 @@ public class Messages extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		processRequest(request, response);
 	}
 
 	/**
